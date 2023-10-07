40f5466ff017dbf26cbc4f1e3a78c27427c8aa8a
ST000044 <zhangqingwei@steincker.com>
2023-10-07 11:27:22 +0800

    提交修改测试代码

--- src/main/java/com/steincker/controller/DemoController.java
+++ src/main/java/com/steincker/controller/DemoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
@@ -13,7 +15,13 @@ public class DemoController {
     @GetMapping("/test")
     @ResponseBody
     public String loop() {
-        return "你好啊，这个是个web项目！ 我再次提交了代码！";
+
+       try {
+           Date.parse("111");
+       }catch (Exception e){
+
+       }
+      return "你好啊，这个是个web项目！ 我再次提交了代码！";
     }
 
 
